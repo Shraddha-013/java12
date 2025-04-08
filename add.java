61eb357169db8c57bf1b35e83565c6115dd6db2e
Shraddha-013 <sjshraddha13@gmail.com>
2025-04-08 11:51:50 +0530

    save

--- add.java
+++ add.java
@@ -4,6 +4,8 @@ public class add {
     public static void main(String[] args) {
         int a=5, b=10;
         int sum = a + b;
+        int diff = a - b;
         System.out.println("Sum of a and b is: " + sum);
+        System.out.println("Difference of a and b is: " + diff);
     }
 }
