6420a4f6765dc927ba3c4229cc13232f102a49d4
Shraddha-013 <sjshraddha13@gmail.com>
2025-04-08 11:48:01 +0530

    save

---
+++ add.java
@@ -0,0 +1,9 @@
+package java1;
+
+public class add {
+    public static void main(String[] args) {
+        int a=5, b=10;
+        int sum = a + b;
+        System.out.println("Sum of a and b is: " + sum);
+    }
+}
